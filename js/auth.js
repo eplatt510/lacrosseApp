20c5be959f4e62e6549c7dc4bf80cb44847f90a7
eplatt510 <eplatt510@gmail.com>
2017-04-01 10:53:48 -0700

    initial commit

---
+++ js/auth.js
@@ -0,0 +1,3 @@
+var clientId = '749600979651-mo5codlm0f395thg2j767v422sncg3rf.apps.googleusercontent.com';
+var apiKey = 'AIzaSyDJH9ZYJgLa12OhJ5OakX9lmi3POoyRJ_E';
+var scopes = 'https://www.googleapis.com/auth/blogger';
